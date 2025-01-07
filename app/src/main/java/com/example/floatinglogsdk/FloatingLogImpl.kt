c536f007703eb8a427b05148154f53f937cdb76c
hch507 <chunghuihan9@gmail.com>
2025-01-07 22:20:32 +0900

    FloatingLogImpl 연결

--- app/src/main/java/com/example/floatinglogsdk/FloatingLogImpl.kt
+++ app/src/main/java/com/example/floatinglogsdk/FloatingLogImpl.kt
@@ -1,18 +1,22 @@
 package com.example.floatinglogsdk
 
+import android.graphics.PixelFormat
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.LifecycleService
 
 
-class FloatingLogImpl: LifecycleService() {
+class FloatingLogImpl : LifecycleService() {
 
 
     private lateinit var windowManager: WindowManager
-    private lateinit var overlayLogView : View
+    private lateinit var overlayLogView: View
     private lateinit var logTextView: TextView
+    private var type: Int? = null
     override fun onCreate() {
         super.onCreate()
 
@@ -22,9 +26,41 @@ class FloatingLogImpl: LifecycleService() {
 
     private fun createFloatingView() {
         val inflater = LayoutInflater.from(this)
-        overlayLogView= inflater.inflate(R.layout.floating_log_view,null)
+        overlayLogView = inflater.inflate(R.layout.floating_log_view, null)
 
+        setType()
+        // LayoutParams 설정
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            type!!,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+            PixelFormat.TRANSLUCENT
+        )
+        initialSet()
+        windowManager.addView(overlayLogView, params)
+    }
+
+    private fun initialSet() {
+        val bt: ImageView = overlayLogView.findViewById(R.id.ib_close)
+        bt.setOnClickListener {
+            stopSelf()
+        }
 
     }
 
+    private fun setType() {
+        type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        else
+            WindowManager.LayoutParams.TYPE_PHONE
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::overlayLogView.isInitialized) {
+            windowManager.removeView(overlayLogView)
+        }
+    }
 }
--- app/src/main/java/com/example/floatinglogsdk/MainActivity.kt
+++ app/src/main/java/com/example/floatinglogsdk/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.floatinglogsdk
 
+import android.app.ComponentCaller
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,10 +24,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun initialSet() {
         mBinding.btStart.setOnClickListener{
-            val intent = Intent(this, FloatingLogImpl::class.java)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+                // Overlay 권한 요청
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, 1234)
             } else {
+                // 권한이 있으면 Service 시작
+                val intent = Intent(this, FloatingLogImpl::class.java)
+                startService(intent)
+            }
+            val intent = Intent(this, FloatingLogImpl::class.java)
+            startService(intent)
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        caller: ComponentCaller
+    ) {
+        if (requestCode == 1234) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                // 권한이 승인되면 Service 시작
+                val intent = Intent(this, FloatingLogImpl::class.java)
                 startService(intent)
             }
         }
